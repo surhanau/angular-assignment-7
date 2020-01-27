fd54cdd92738005d09b91cd39e9579b90b4d8673
Ivan Surhanau <ivan@surhanau.org>
2020-01-27 18:39:20 +0300

    Assignment implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    projectForm: FormGroup;
+    statuses: Array<string> = ['Stable', 'Finished', 'Critical'];
+    deprecatedProjectNames: Array<string> = ['test'];
+
+    ngOnInit(): void {
+        this.projectForm = new FormGroup({
+            'projectData': new FormGroup({
+                // // Non-async project name validator.
+                // 'name': new FormControl(
+                //     null,
+                //     [Validators.required, this.validateProjectName.bind(this)]),
+                'name': new FormControl(
+                    null,
+                    [Validators.required],
+                    [this.validateProjectNameAsync]),
+                'email': new FormControl(null, [Validators.required, Validators.email]),
+                'status': new FormControl('Finished', [Validators.required]),
+            }),
+        });
+    }
+
+    onSubmit(): void {
+        console.log(this.projectForm.value);
+    }
+
+    validateProjectName(control: FormControl): { [str: string]: boolean } {
+        if (this.deprecatedProjectNames.indexOf(String(control.value).toLowerCase()) !== -1) {
+            return { 'deprecatedProjectName': true };
+        }
+        return null;
+    }
+
+    validateProjectNameAsync(control: FormControl): Promise<any> | Observable<any> {
+        return new Promise<any>((resolve, reject) => {
+            setTimeout(() => {
+                if (String(control.value).toLowerCase() === 'test') {
+                    resolve({ 'deprecatedProjectName': true });
+                } else {
+                    resolve(null);
+                }
+            }, 100);
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ProjectNameValidator } from './Validators/project-name.validator';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
